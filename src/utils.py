f4404096f39621d509d07e1364cdc5166a5c0023
Andrey Osipov <bezy4uy@gmail.com>
2025-01-23 18:51:00 +0300

    added event_page functionality of expenses calculating

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import time
 from typing import Any
 
+import numpy as np
 import pandas as pd
 import requests
 
@@ -105,6 +106,96 @@ def top_five_by_trans_amount(
     return sorted_transactions_by_time
 
 
+def expenses_calculator(df: pd.DataFrame, transactions_date: str) -> dict[Any, Any]:
+    actual_date = time.strptime(transactions_date, "%Y-%m-%d %H:%M:%S")
+
+    amount = 0
+
+    category_list: list[dict[str, Any]] = []
+
+    nan_check_summ = df["Сумма операции"].notnull()
+    nan_check_cat = df["Категория"].notnull()
+
+    for i in df.index:
+        if time.strptime(df.loc[i, "Дата операции"].item(), "%d.%m.%Y %H:%M:%S") < actual_date:
+            if df.loc[i, "Сумма операции"] < 0:
+                amount += df.loc[i, "Сумма операции"].item()
+
+                if nan_check_summ[i] and nan_check_cat[i]:
+
+                    found = False
+
+                    for cat in category_list:
+                        if df.loc[i, "Категория"] == cat["category"]:
+                            cat["amount"] += df.loc[i, "Сумма операции"].item()
+                            found = True
+                            break
+
+                    if not found:
+                        category_list.append(
+                            {"category": df.loc[i, "Категория"], "amount": df.loc[i, "Сумма операции"].item()}
+                        )
+
+                else:
+                    found = False
+
+                    for cat in category_list:
+                        if cat["category"] == "Остальное":
+                            cat["amount"] += df.loc[i, "Сумма операции"].item()
+                            found = True
+                            break
+
+                    if not found:
+                        category_list.append({"category": "Остальное", "amount": df.loc[i, "Сумма операции"].item()})
+
+    total_amount = round(amount, 2)
+
+    transfers_and_cash = []
+
+    category_other = dict()
+
+    for el in category_list:
+        if el["category"] == "Наличные" or el["category"] == "Переводы":
+            el["amount"] = round(el["amount"], 2)
+            transfers_and_cash.append(el)
+            category_list.remove(el)
+
+        elif el["category"] == "Остальное":
+            el["amount"] = round(el["amount"], 2)
+            category_other = el
+            category_list.remove(el)
+
+    category_list.sort(key=lambda x: x["amount"])
+
+    main = []
+
+    for elem in category_list[:7]:
+        main.append({"category": elem["category"], "amount": round(elem["amount"], 2)})
+
+    main.append(category_other)
+
+    expenses = {
+        "total_amount": total_amount,
+        "main": main,
+        "transfers_and_cash": [
+            {
+                "category": "Наличные",
+                "amount": next(el["amount"] for el in transfers_and_cash if el["category"] == "Наличные"),
+            },
+            {
+                "category": "Переводы",
+                "amount": next(el["amount"] for el in transfers_and_cash if el["category"] == "Переводы"),
+            },
+        ],
+    }
+
+    return expenses
+
+
+def income_calculator(df: pd.DataFrame, transactions_date: str) -> dict[Any, Any]:
+    return dict()
+
+
 def exchange_rate(user_data: dict[str, list[str]]) -> list[dict[str, float]]:  # готова (Спрятать API)
     """Информация по курсу валют"""
 
@@ -123,12 +214,185 @@ def exchange_rate(user_data: dict[str, list[str]]) -> list[dict[str, float]]:  #
 
     currency_list = []
 
-    currencys = {'success': True, 'timestamp': 1737614523, 'source': 'RUB', 'quotes': {'RUBAED': 0.037009, 'RUBAFN': 0.760069, 'RUBALL': 0.952014, 'RUBAMD': 4.057924, 'RUBANG': 0.018228, 'RUBAOA': 9.204359, 'RUBARS': 10.552191, 'RUBAUD': 0.016072, 'RUBAWG': 0.018162, 'RUBAZN': 0.017055, 'RUBBAM': 0.018941, 'RUBBBD': 0.02042, 'RUBBDT': 1.233357, 'RUBBGN': 0.018938, 'RUBBHD': 0.003797, 'RUBBIF': 29.926552, 'RUBBMD': 0.010076, 'RUBBND': 0.013686, 'RUBBOB': 0.069885, 'RUBBRL': 0.059889, 'RUBBSD': 0.010114, 'RUBBTC': 9.8209e-08, 'RUBBTN': 0.874392, 'RUBBWP': 0.139887, 'RUBBYN': 0.033098, 'RUBBYR': 197.488626, 'RUBBZD': 0.020316, 'RUBCAD': 0.014498, 'RUBCDF': 28.66608, 'RUBCHF': 0.009132, 'RUBCLF': 0.000362, 'RUBCLP': 9.988391, 'RUBCNY': 0.073357, 'RUBCNH': 0.073386, 'RUBCOP': 42.933624, 'RUBCRC': 5.086496, 'RUBCUC': 0.010076, 'RUBCUP': 0.267013, 'RUBCVE': 1.067892, 'RUBCZK': 0.243431, 'RUBDJF': 1.800982, 'RUBDKK': 0.072221, 'RUBDOP': 0.620607, 'RUBDZD': 1.364573, 'RUBEGP': 0.507117, 'RUBERN': 0.151139, 'RUBETB': 1.293729, 'RUBEUR': 0.009681, 'RUBFJD': 0.023314, 'RUBFKP': 0.008298, 'RUBGBP': 0.008182, 'RUBGEL': 0.028819, 'RUBGGP': 0.008298, 'RUBGHS': 0.153223, 'RUBGIP': 0.008298, 'RUBGMD': 0.735545, 'RUBGNF': 87.426207, 'RUBGTQ': 0.078175, 'RUBGYD': 2.114963, 'RUBHKD': 0.078497, 'RUBHNL': 0.257482, 'RUBHRK': 0.074356, 'RUBHTG': 1.320739, 'RUBHUF': 3.978484, 'RUBIDR': 163.752833, 'RUBILS': 0.035725, 'RUBIMP': 0.008298, 'RUBINR': 0.871041, 'RUBIQD': 13.248654, 'RUBIRR': 424.197506, 'RUBISK': 1.414357, 'RUBJEP': 0.008298, 'RUBJMD': 1.586964, 'RUBJOD': 0.007148, 'RUBJPY': 1.577133, 'RUBKES': 1.302316, 'RUBKGS': 0.881141, 'RUBKHR': 40.773716, 'RUBKMF': 4.761876, 'RUBKPW': 9.068356, 'RUBKRW': 14.483826, 'RUBKWD': 0.003106, 'RUBKYD': 0.008429, 'RUBKZT': 5.268521, 'RUBLAK': 220.609648, 'RUBLBP': 905.681101, 'RUBLKR': 3.020199, 'RUBLRD': 2.002503, 'RUBLSL': 0.186857, 'RUBLTL': 0.029752, 'RUBLVL': 0.006095, 'RUBLYD': 0.049751, 'RUBMAD': 0.100917, 'RUBMDL': 0.188621, 'RUBMGA': 47.407463, 'RUBMKD': 0.59561, 'RUBMMK': 32.726294, 'RUBMNT': 34.23808, 'RUBMOP': 0.08113, 'RUBMRU': 0.402833, 'RUBMUR': 0.468732, 'RUBMVR': 0.155273, 'RUBMWK': 17.537372, 'RUBMXN': 0.206556, 'RUBMYR': 0.044773, 'RUBMZN': 0.643958, 'RUBNAD': 0.186857, 'RUBNGN': 15.707603, 'RUBNIO': 0.372193, 'RUBNOK': 0.113698, 'RUBNPR': 1.399058, 'RUBNZD': 0.017785, 'RUBOMR': 0.003879, 'RUBPAB': 0.010114, 'RUBPEN': 0.037724, 'RUBPGK': 0.04118, 'RUBPHP': 0.590939, 'RUBPKR': 2.820205, 'RUBPLN': 0.04089, 'RUBPYG': 79.996299, 'RUBQAR': 0.036913, 'RUBRON': 0.04817, 'RUBRSD': 1.133887, 'RUBRWF': 14.18931, 'RUBSAR': 0.037797, 'RUBSBD': 0.085391, 'RUBSCR': 0.149359, 'RUBSDG': 6.055649, 'RUBSEK': 0.110934, 'RUBSGD': 0.013665, 'RUBSHP': 0.008298, 'RUBSLE': 0.228642, 'RUBSLL': 211.287639, 'RUBSOS': 5.779933, 'RUBSRD': 0.353466, 'RUBSTD': 208.551828, 'RUBSVC': 0.0885, 'RUBSYP': 131.007506, 'RUBSZL': 0.186713, 'RUBTHB': 0.341943, 'RUBTJS': 0.110693, 'RUBTMT': 0.035367, 'RUBTND': 0.032101, 'RUBTOP': 0.023599, 'RUBTRY': 0.359331, 'RUBTTD': 0.068704, 'RUBTWD': 0.329811, 'RUBTZS': 25.431699, 'RUBUAH': 0.424784, 'RUBUGX': 37.218617, 'RUBUSD': 0.010076, 'RUBUYU': 0.442599, 'RUBUZS': 131.374067, 'RUBVES': 0.561023, 'RUBVND': 252.452935, 'RUBVUV': 1.196237, 'RUBWST': 0.028221, 'RUBXAF': 6.352862, 'RUBXAG': 0.000328, 'RUBXAU': 3.65797e-06, 'RUBXCD': 0.027231, 'RUBXDR': 0.007792, 'RUBXOF': 6.352801, 'RUBXPF': 1.154996, 'RUBYER': 2.509417, 'RUBZAR': 0.186626, 'RUBZMK': 90.695636, 'RUBZMW': 0.281924, 'RUBZWL': 3.244452}}
+    currencys = {
+        "success": True,
+        "timestamp": 1737614523,
+        "source": "RUB",
+        "quotes": {
+            "RUBAED": 0.037009,
+            "RUBAFN": 0.760069,
+            "RUBALL": 0.952014,
+            "RUBAMD": 4.057924,
+            "RUBANG": 0.018228,
+            "RUBAOA": 9.204359,
+            "RUBARS": 10.552191,
+            "RUBAUD": 0.016072,
+            "RUBAWG": 0.018162,
+            "RUBAZN": 0.017055,
+            "RUBBAM": 0.018941,
+            "RUBBBD": 0.02042,
+            "RUBBDT": 1.233357,
+            "RUBBGN": 0.018938,
+            "RUBBHD": 0.003797,
+            "RUBBIF": 29.926552,
+            "RUBBMD": 0.010076,
+            "RUBBND": 0.013686,
+            "RUBBOB": 0.069885,
+            "RUBBRL": 0.059889,
+            "RUBBSD": 0.010114,
+            "RUBBTC": 9.8209e-08,
+            "RUBBTN": 0.874392,
+            "RUBBWP": 0.139887,
+            "RUBBYN": 0.033098,
+            "RUBBYR": 197.488626,
+            "RUBBZD": 0.020316,
+            "RUBCAD": 0.014498,
+            "RUBCDF": 28.66608,
+            "RUBCHF": 0.009132,
+            "RUBCLF": 0.000362,
+            "RUBCLP": 9.988391,
+            "RUBCNY": 0.073357,
+            "RUBCNH": 0.073386,
+            "RUBCOP": 42.933624,
+            "RUBCRC": 5.086496,
+            "RUBCUC": 0.010076,
+            "RUBCUP": 0.267013,
+            "RUBCVE": 1.067892,
+            "RUBCZK": 0.243431,
+            "RUBDJF": 1.800982,
+            "RUBDKK": 0.072221,
+            "RUBDOP": 0.620607,
+            "RUBDZD": 1.364573,
+            "RUBEGP": 0.507117,
+            "RUBERN": 0.151139,
+            "RUBETB": 1.293729,
+            "RUBEUR": 0.009681,
+            "RUBFJD": 0.023314,
+            "RUBFKP": 0.008298,
+            "RUBGBP": 0.008182,
+            "RUBGEL": 0.028819,
+            "RUBGGP": 0.008298,
+            "RUBGHS": 0.153223,
+            "RUBGIP": 0.008298,
+            "RUBGMD": 0.735545,
+            "RUBGNF": 87.426207,
+            "RUBGTQ": 0.078175,
+            "RUBGYD": 2.114963,
+            "RUBHKD": 0.078497,
+            "RUBHNL": 0.257482,
+            "RUBHRK": 0.074356,
+            "RUBHTG": 1.320739,
+            "RUBHUF": 3.978484,
+            "RUBIDR": 163.752833,
+            "RUBILS": 0.035725,
+            "RUBIMP": 0.008298,
+            "RUBINR": 0.871041,
+            "RUBIQD": 13.248654,
+            "RUBIRR": 424.197506,
+            "RUBISK": 1.414357,
+            "RUBJEP": 0.008298,
+            "RUBJMD": 1.586964,
+            "RUBJOD": 0.007148,
+            "RUBJPY": 1.577133,
+            "RUBKES": 1.302316,
+            "RUBKGS": 0.881141,
+            "RUBKHR": 40.773716,
+            "RUBKMF": 4.761876,
+            "RUBKPW": 9.068356,
+            "RUBKRW": 14.483826,
+            "RUBKWD": 0.003106,
+            "RUBKYD": 0.008429,
+            "RUBKZT": 5.268521,
+            "RUBLAK": 220.609648,
+            "RUBLBP": 905.681101,
+            "RUBLKR": 3.020199,
+            "RUBLRD": 2.002503,
+            "RUBLSL": 0.186857,
+            "RUBLTL": 0.029752,
+            "RUBLVL": 0.006095,
+            "RUBLYD": 0.049751,
+            "RUBMAD": 0.100917,
+            "RUBMDL": 0.188621,
+            "RUBMGA": 47.407463,
+            "RUBMKD": 0.59561,
+            "RUBMMK": 32.726294,
+            "RUBMNT": 34.23808,
+            "RUBMOP": 0.08113,
+            "RUBMRU": 0.402833,
+            "RUBMUR": 0.468732,
+            "RUBMVR": 0.155273,
+            "RUBMWK": 17.537372,
+            "RUBMXN": 0.206556,
+            "RUBMYR": 0.044773,
+            "RUBMZN": 0.643958,
+            "RUBNAD": 0.186857,
+            "RUBNGN": 15.707603,
+            "RUBNIO": 0.372193,
+            "RUBNOK": 0.113698,
+            "RUBNPR": 1.399058,
+            "RUBNZD": 0.017785,
+            "RUBOMR": 0.003879,
+            "RUBPAB": 0.010114,
+            "RUBPEN": 0.037724,
+            "RUBPGK": 0.04118,
+            "RUBPHP": 0.590939,
+            "RUBPKR": 2.820205,
+            "RUBPLN": 0.04089,
+            "RUBPYG": 79.996299,
+            "RUBQAR": 0.036913,
+            "RUBRON": 0.04817,
+            "RUBRSD": 1.133887,
+            "RUBRWF": 14.18931,
+            "RUBSAR": 0.037797,
+            "RUBSBD": 0.085391,
+            "RUBSCR": 0.149359,
+            "RUBSDG": 6.055649,
+            "RUBSEK": 0.110934,
+            "RUBSGD": 0.013665,
+            "RUBSHP": 0.008298,
+            "RUBSLE": 0.228642,
+            "RUBSLL": 211.287639,
+            "RUBSOS": 5.779933,
+            "RUBSRD": 0.353466,
+            "RUBSTD": 208.551828,
+            "RUBSVC": 0.0885,
+            "RUBSYP": 131.007506,
+            "RUBSZL": 0.186713,
+            "RUBTHB": 0.341943,
+            "RUBTJS": 0.110693,
+            "RUBTMT": 0.035367,
+            "RUBTND": 0.032101,
+            "RUBTOP": 0.023599,
+            "RUBTRY": 0.359331,
+            "RUBTTD": 0.068704,
+            "RUBTWD": 0.329811,
+            "RUBTZS": 25.431699,
+            "RUBUAH": 0.424784,
+            "RUBUGX": 37.218617,
+            "RUBUSD": 0.010076,
+            "RUBUYU": 0.442599,
+            "RUBUZS": 131.374067,
+            "RUBVES": 0.561023,
+            "RUBVND": 252.452935,
+            "RUBVUV": 1.196237,
+            "RUBWST": 0.028221,
+            "RUBXAF": 6.352862,
+            "RUBXAG": 0.000328,
+            "RUBXAU": 3.65797e-06,
+            "RUBXCD": 0.027231,
+            "RUBXDR": 0.007792,
+            "RUBXOF": 6.352801,
+            "RUBXPF": 1.154996,
+            "RUBYER": 2.509417,
+            "RUBZAR": 0.186626,
+            "RUBZMK": 90.695636,
+            "RUBZMW": 0.281924,
+            "RUBZWL": 3.244452,
+        },
+    }
 
     for currency in user_data["user_currencies"]:
-        currency_list.append(
-            {"currency": currency, "rate": round(1 / currencys["quotes"][f"RUB{currency}"], 2)}
-        )
+        currency_list.append({"currency": currency, "rate": round(1 / currencys["quotes"][f"RUB{currency}"], 2)})
 
     return currency_list
 
@@ -137,7 +401,78 @@ def stock_price(user_data: dict[str, list[str]]) -> list[dict[str, str]]:  # г�
     """Стоимость акций из S&P500"""
 
     stock_prices = []
-    stock_list = [{'Global Quote': {'01. symbol': 'AAPL', '02. open': '219.7900', '03. high': '224.1200', '04. low': '219.7900', '05. price': '223.8300', '06. volume': '64126500', '07. latest trading day': '2025-01-22', '08. previous close': '222.6400', '09. change': '1.1900', '10. change percent': '0.5345%'}}, {'Global Quote': {'01. symbol': 'AMZN', '02. open': '232.0200', '03. high': '235.4400', '04. low': '231.1900', '05. price': '235.0100', '06. volume': '41448217', '07. latest trading day': '2025-01-22', '08. previous close': '230.7100', '09. change': '4.3000', '10. change percent': '1.8638%'}}, {'Global Quote': {'01. symbol': 'GOOGL', '02. open': '199.0600', '03. high': '200.4800', '04. low': '197.5300', '05. price': '198.3700', '06. volume': '26200617', '07. latest trading day': '2025-01-22', '08. previous close': '198.0500', '09. change': '0.3200', '10. change percent': '0.1616%'}}, {'Global Quote': {'01. symbol': 'MSFT', '02. open': '437.5600', '03. high': '447.2700', '04. low': '436.0000', '05. price': '446.2000', '06. volume': '27803811', '07. latest trading day': '2025-01-22', '08. previous close': '428.5000', '09. change': '17.7000', '10. change percent': '4.1307%'}}, {'Global Quote': {'01. symbol': 'TSLA', '02. open': '416.8100', '03. high': '428.0000', '04. low': '414.5900', '05. price': '415.1100', '06. volume': '58585801', '07. latest trading day': '2025-01-22', '08. previous close': '424.0700', '09. change': '-8.9600', '10. change percent': '-2.1129%'}}]
+    stock_list = [
+        {
+            "Global Quote": {
+                "01. symbol": "AAPL",
+                "02. open": "219.7900",
+                "03. high": "224.1200",
+                "04. low": "219.7900",
+                "05. price": "223.8300",
+                "06. volume": "64126500",
+                "07. latest trading day": "2025-01-22",
+                "08. previous close": "222.6400",
+                "09. change": "1.1900",
+                "10. change percent": "0.5345%",
+            }
+        },
+        {
+            "Global Quote": {
+                "01. symbol": "AMZN",
+                "02. open": "232.0200",
+                "03. high": "235.4400",
+                "04. low": "231.1900",
+                "05. price": "235.0100",
+                "06. volume": "41448217",
+                "07. latest trading day": "2025-01-22",
+                "08. previous close": "230.7100",
+                "09. change": "4.3000",
+                "10. change percent": "1.8638%",
+            }
+        },
+        {
+            "Global Quote": {
+                "01. symbol": "GOOGL",
+                "02. open": "199.0600",
+                "03. high": "200.4800",
+                "04. low": "197.5300",
+                "05. price": "198.3700",
+                "06. volume": "26200617",
+                "07. latest trading day": "2025-01-22",
+                "08. previous close": "198.0500",
+                "09. change": "0.3200",
+                "10. change percent": "0.1616%",
+            }
+        },
+        {
+            "Global Quote": {
+                "01. symbol": "MSFT",
+                "02. open": "437.5600",
+                "03. high": "447.2700",
+                "04. low": "436.0000",
+                "05. price": "446.2000",
+                "06. volume": "27803811",
+                "07. latest trading day": "2025-01-22",
+                "08. previous close": "428.5000",
+                "09. change": "17.7000",
+                "10. change percent": "4.1307%",
+            }
+        },
+        {
+            "Global Quote": {
+                "01. symbol": "TSLA",
+                "02. open": "416.8100",
+                "03. high": "428.0000",
+                "04. low": "414.5900",
+                "05. price": "415.1100",
+                "06. volume": "58585801",
+                "07. latest trading day": "2025-01-22",
+                "08. previous close": "424.0700",
+                "09. change": "-8.9600",
+                "10. change percent": "-2.1129%",
+            }
+        },
+    ]
     counter = 0
 
     for symbol in user_data["user_stocks"]:
--- src/views.py
+++ src/views.py
@@ -1,7 +1,17 @@
 import json
 from typing import Any
+import pandas as pd
 
-from src.utils import card_information, exchange_rate, greetings, read_xls, stock_price, top_five_by_trans_amount
+from src.utils import (
+    card_information, exchange_rate, greetings, read_xls, stock_price,
+    top_five_by_trans_amount, expenses_calculator, income_calculator
+)
+
+with open("user_settings.json", "r") as file:
+    user_data = json.load(file)
+
+file_info = read_xls("data\\operations.xlsx")
+file_pd_info = pd.read_excel("data\\operations.xlsx")
 
 
 # Функция для страницы "Главная"
@@ -15,17 +25,11 @@ def home_page(date_time_string: str) -> dict[str, Any]:  # готова
         5. Стоимость акций из S&p500
     """
 
-    file_info = read_xls("data\\operations.xlsx")
-
     greet_string = greetings(date_time_string)  # 1 Приветствие
 
     card_info = card_information(file_info)  # 2 Инфо по карте
 
     top_5 = top_five_by_trans_amount(date_time_string, file_info)  # 3 Top 5 by summ
-
-    with open("user_settings.json", "r") as file:
-        data = json.load(file)
-        user_data = data
 
     rate = exchange_rate(user_data)  # 4 Курс валют
 
@@ -45,7 +49,7 @@ def home_page(date_time_string: str) -> dict[str, Any]:  # готова
 # ---------------------------------------------------------------------------------------------------------------------
 
 
-#def event_page(dataframe):  # на вход идет датафрейм
+# def event_page(dataframe):  # на вход идет датафрейм
 def event_page(date_time_string: str, data_range: str = "M") -> json:  # на вход идет дата и необязательный параметр
     """
     Принимает на вход дату и параметр диапазона, на выходе выдавая:
@@ -55,22 +59,20 @@ def event_page(date_time_string: str, data_range: str = "M") -> json:  # на в
         4. Стоимость акций из S&P 500
     """
 
-    expenses = None  # 1. Расходы
-    total_outcome = 0
-    transfer_and_cash = None
+    expenses = expenses_calculator(file_pd_info, date_time_string)  # 1. Расходы
 
-    receipts = None  # 2. Поступления
+    receipts = income_calculator(file_pd_info, date_time_string)  # 2. Поступления
     total_income = 0
 
-    rate = exchange_rate()  # 3. Курс валют
+    rate = exchange_rate(user_data)  # 3. Курс валют
 
-    stock_prices = stock_price()  # 4. Стоимость акций
+    stock_prices = stock_price(user_data)  # 4. Стоимость акций
 
     event_page_answer = {
         "expenses": {
-            "total amount": total_outcome,
-            "main": [expenses],
-            "transfers_and_cash": [transfer_and_cash]
+            "total amount": expenses["total_amount"],
+            "main": expenses["main"],
+            "transfers_and_cash": expenses["transfers_and_cash"]
         },
         "income": {
             "total_amount": total_income,
@@ -81,4 +83,4 @@ def event_page(date_time_string: str, data_range: str = "M") -> json:  # на в
 
     }
 
-    return event_page_answer  # возвращается корректный JSON-ответ согласно ТЗ
+    return event_page_answer  # event_page_answer  # возвращается корректный JSON-ответ согласно ТЗ
